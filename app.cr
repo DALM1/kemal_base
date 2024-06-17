8f3a48ead0bd4c464c8cc0d553e5299dec5a6296
Dimitri Almon <dimitri.almon@gmail.com>
2024-06-17 23:44:12 +0200

    Updates base

--- app.cr
+++ app.cr
@@ -1,7 +1,31 @@
 require "kemal"
+require "ecr"
 
-get "/" do
-  "Hello, World"
+# Serve static files from the "public" folder
+Kemal.config.public_folder = "public"
+
+get "/" do |env|
+  html = ECR.render("src/views/index.ecr")
+  env.response.content_type = "text/html"
+  html
+end
+
+get "/about" do |env|
+  html = <<-HTML
+  <html>
+    <head>
+      <title>About</title>
+      <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </body>
+  </html>
+  HTML
+
+  env.response.content_type = "text/html"
+  html
 end
 
 Kemal.run
